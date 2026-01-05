a1bc5db6fe02c4cae15347e8a309962676b48ae7
luanahmetaj <luan.ahmetaj@adriusgroup.com>
2026-01-05 22:54:17 +0100

    Minor fix

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import * as AccordionPrimitive from "@radix-ui/react-accordion";
-import { ChevronDownIcon } from "lucide-react@0.487.0";
+import { ChevronDownIcon } from "lucide-react";
 
 import { cn } from "./utils";
 
